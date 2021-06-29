62c85c0a5de0ee416a805c225e6970abef54d31a
lalithh24 <85875721+lalithh24@users.noreply.github.com>
2021-06-29 12:55:31 +0530

    Update string_functions.py

--- string_functions.py
+++ string_functions.py
@@ -1,1 +1,8 @@
+
 sample_string = "hello  there!"
+print(len(sample_string))        #prints the number of characters in the string; including spaces and punctuation
+print(sample_string.index("h"))  #prints the element in the location referring to [2]
+print(sample_string.count('l'))  #prints the number of occurences of the character "l"
+print(sample_string[2:5])        #prints characters starting from 3 upto 7, but not 7
+print(sample_string[:])          #prints the entire string
+print(sample_string[:9])     
