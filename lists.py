ba5cbeffa16ca10b87098fe4c23b975be6c8c4e6
lalithh24 <85875721+lalithh24@users.noreply.github.com>
2021-06-29 12:18:34 +0530

    Basic list appending

--- lists.py
+++ lists.py
@@ -1,8 +1,20 @@
-my_list = [] #initialsing an empty list 
+my_list = []      #initialsing an empty list 
 
-my_list.append(50)
+my_list.append(50)   #append adds a new item at the end of the list 
 my_list.append(69)
 my_list.append(68)
-print(my_list)
+
+print(my_list)     #all items at once
+print(my_list[0])  #prints the first value in the list through indexing.
+
+
+
+#using for loop, we can print the same 
+
+for x in my_list:
+  print(x)
+  
+  
+
 
 
