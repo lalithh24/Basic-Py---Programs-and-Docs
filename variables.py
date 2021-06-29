996cefeb7ad8a4eef63423507fb2e7473ebfae3e
lalithh24 <85875721+lalithh24@users.noreply.github.com>
2021-06-29 12:04:36 +0530

    Create variables.py
    Basic operations on strings and numbers with variables

---
+++ variables.py
@@ -0,0 +1,26 @@
+#declaring variables and some operations on them
+
+my_int = 5               #declares my_int as an integer data type and assigns a value of 5
+my_float = 6.0           #declares my_float as a float data type and assigns a value of 6.0
+my_string = "newstring"  #declares my_string as a string data type and assigns a value of 'newstring'
+
+print(my_int)            #displays 5 as output
+print(my_float)          #displays 6.0 as output
+print(my_string)         #displays newstring as output 
+
+#operators on strings and numbers
+
+# "+" in numbers is mathematical addition
+
+a = 5
+b = 6
+d,e = 4,40 #can declare simultaneously also
+c = a+b+d+e
+print("the sum is:",c)
+
+#  "+" in strings is concatenation
+string_one = "hello"
+string_two = "world"
+string_three = string_one + ' ' + string_two
+print("the string after concatenation is:" string_three)
+
